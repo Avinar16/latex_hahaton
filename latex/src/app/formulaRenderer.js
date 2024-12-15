b55e7472c55b0c76896e43dbe1fa3b4e80e26c75
jammbeast <jammbeast69@mail.ru>
2024-12-15 20:33:41 +0300

    init-2

--- latex/src/app/formulaRenderer.js
+++ latex/src/app/formulaRenderer.js
@@ -1,15 +1,35 @@
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 export default function formulaRenderer({latex}) {
-    
+
+  const handleSubmit = () => {
+
+    const payload = JSON.stringify({ formula: latex });
+    console.log('это отправляем на сервер:', payload);
+    fetch('/api/analysis/', {
+     method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: payload, 
+      
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  };
   
     return (
         <MathJaxContext>
 <div>
         <h3>Рендеринг формулы:</h3>
-        <MathJax >{`$$${latex}$$`}</MathJax>
+        <MathJax >{`${latex}`}</MathJax>
         <div>
-          <button onClick={() => alert(latex)}>Сохранить формулу</button>
+          <button onClick={handleSubmit}>Сохранить формулу</button>
         </div>
         </div>
         </MathJaxContext>
